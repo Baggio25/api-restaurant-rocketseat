d3cfa32480b2c897d029163c1ded7891d363b2d5
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-15 21:27:20 -0300

    Configurando o projeto

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const PORT = 3333;
+
+const app = express();
+app.use(express.json());
+app.listen(PORT, () => console.log(`**** Server is learning on PORT: ${PORT} ****`));
